07d3f16b29fba41cb809a71f78afd05b6ae1872b
thleima <thomas61.amiel@gmail.com>
2025-03-14 19:33:25 +0100

    excercise finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,59 +22,80 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-	res.send(JSON.stringify(books, null, 4));
+public_users.get("/", async function (req, res) {
+	try {
+		res.send(JSON.stringify(books, null, 4));
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
+	}
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
 	const isbn = req.params.isbn;
-	if (books[isbn]) {
-		res.send(JSON.stringify(books[isbn], null, 4));
-	} else {
-		res.status(404).json({ message: "Book not found" });
+	try {
+		if (books[isbn]) {
+			res.send(JSON.stringify(books[isbn], null, 4));
+		} else {
+			res.status(404).json({ message: "Book not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
 	const author = req.params.author;
 	let author_books = {};
-	for (let book in books) {
-		if (parsedString(books[book].author) === parsedString(author)) {
-			author_books[book] = books[book];
+	try {
+		for (let book in books) {
+			if (parsedString(books[book].author) === parsedString(author)) {
+				author_books[book] = books[book];
+			}
 		}
-	}
-	if (Object.keys(author_books).length > 0) {
-		res.send(JSON.stringify(author_books, null, 4));
-	} else {
-		res.status(404).json({ message: "Author not found" });
+		if (Object.keys(author_books).length > 0) {
+			res.send(JSON.stringify(author_books, null, 4));
+		} else {
+			res.status(404).json({ message: "Author not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
 	const title = req.params.title;
 	let title_books = {};
-	for (let book in books) {
-		if (parsedString(books[book].title) === parsedString(title)) {
-			title_books[book] = books[book];
+	try {
+		for (let book in books) {
+			if (parsedString(books[book].title) === parsedString(title)) {
+				title_books[book] = books[book];
+			}
 		}
-	}
-	if (Object.keys(title_books).length > 0) {
-		res.send(JSON.stringify(title_books, null, 4));
-	} else {
-		res.status(404).json({ message: "Title not found" });
+		if (Object.keys(title_books).length > 0) {
+			res.send(JSON.stringify(title_books, null, 4));
+		} else {
+			res.status(404).json({ message: "Title not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
-//  Get book review
-public_users.get("/review/:isbn", function (req, res) {
-	const reviews = books[req.params.isbn].reviews;
-	if (reviews) {
-		res.send(JSON.stringify(reviews, null, 4));
-	} else {
-		res.status(404).json({ message: "Book not found" });
+// Get book review
+public_users.get("/review/:isbn", async function (req, res) {
+	const isbn = req.params.isbn;
+	try {
+		const reviews = books[isbn].reviews;
+		if (reviews) {
+			res.send(JSON.stringify(reviews, null, 4));
+		} else {
+			res.status(404).json({ message: "Book not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server Error" });
 	}
 });
 
